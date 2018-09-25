0b1813e2f9e913d9e8637794120ecbe27649077d
ke <keye@leoedu.com>
2018-09-25 15:32:50 +0800

    error handing

--- app/routes.py
+++ app/routes.py
@@ -6,6 +6,7 @@ from app.models import User
 from werkzeug.urls import url_parse
 from datetime import datetime
 
+
 @app.route("/")
 @app.route("/index")
 def index():
@@ -23,7 +24,7 @@ def index():
     return render_template("index.html", title="Home", posts=posts)
 
 
-@app.route("/login", methods=["GET","POST"])
+@app.route("/login", methods=["GET", "POST"])
 def login():
     if current_user.is_authenticated:
         return redirect(url_for("index"))
@@ -48,6 +49,7 @@ def login():
 def logout():
     logout_user()
     return redirect(url_for("index"))
+
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
@@ -87,7 +89,7 @@ def before_request():
 @app.route("/edit_profile", methods=["GET", "POST"])
 @login_required
 def edit_profile():
-    form = EditProfileForm()
+    form = EditProfileForm(current_user.username)
     if form.validate_on_submit():
         current_user.username = form.username.data
         current_user.about_me = form.about_me.data
@@ -98,12 +100,3 @@ def edit_profile():
         form.username.data = current_user.username
         form.about_me.data = current_user.about_me
     return render_template("edit_profile.html", title="Edit Profile", form=form)
-
-
-
-
-
-
-
-
-
--- config.py
+++ config.py
@@ -3,8 +3,14 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 print(basedir)
 
+
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-                              'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    MAIL_SERVER = os.environ.get("MAIL_SERVER") or "smtp.163.com"
+    MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
+    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME') or "h123apk@163.com"
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    ADMINS = ['744194305@qq.com']
