0f234e3d2b268fbe18fd9bdfa52fe8f02039f900
ke <keye@leoedu.com>
2018-09-25 18:34:27 +0800

    continue

--- app/models.py
+++ app/models.py
@@ -4,9 +4,10 @@ from datetime import datetime
 from app import db, login
 from hashlib import md5
 
-@login.user_loader
-def load_user(id):
-    return User.query.get(int(id))
+followers = db.Table('followers',
+                     db.Column('follower_id', db.Integer, db.ForeignKey("user.id")),
+                     db.Column('followed_id', db.Integer, db.ForeignKey("user.id")),
+                     )
 
 
 class User(db.Model, UserMixin):
@@ -14,9 +15,17 @@ class User(db.Model, UserMixin):
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(128), index=True, unique=True)
     password_hash = db.Column(db.String(128))
-    posts= db.relationship('Post', backref='author', lazy='dynamic')
+    posts = db.relationship('Post', backref='author', lazy='dynamic')
     about_me = db.Column(db.String(140))
     last_seen = db.Column(db.DateTime, default=datetime.utcnow)
+    followed = db.relationship(
+        'User',
+        secondary=followers,
+        primaryjoin=(followers.c.follower_id == id),
+        secondaryjoin=(followers.c.followed_id == id),
+        backref=db.backref('followers', lazy='dynamic'),
+        lazy='dynamic'
+    )
 
     def __repr__(self):
         return "<User {}>".format(self.username)
@@ -32,6 +41,31 @@ class User(db.Model, UserMixin):
         return 'https://cdn.v2ex.com/gravatar/{}?d=identicon&s={}'.format(
             digest, size)
 
+    def follow(self, user):
+        if not self.is_following(user):
+            self.followed.append(user)
+
+    def unfollow(self, user):
+        if self.is_following(user):
+            self.followed.remove(user)
+
+    def is_following(self, user):
+        return self.followed.filter(
+            followers.c.followed_id == user.id
+        ).count() > 0
+
+    def followed_posts(self):
+        followed = Post.query.join(
+            followers, (followers.c.followed_id == Post.user_id)).filter(
+            followers.c.follower_id == self.id)
+        own = Post.query.filter_by(user_id=self.id)
+        return followed.union(own).order_by(Post.timestamp.desc())
+
+
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
+
 
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -42,6 +76,6 @@ class Post(db.Model):
     def __repr__(self):
         return "<Post {}>".format(self.body)
 
+
 if __name__ == '__main__':
     print(User().password_hash(1))
-
